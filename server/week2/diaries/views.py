8825a9f79ab5df8aa60ba79cc794147d37becea2
woowonjin <2017029970@hanyang.ac.kr>
2021-01-15 00:23:07 +0900

    Diary Server

--- server/week2/diaries/views.py
+++ server/week2/diaries/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
+from users import models as user_models
+import json
 # Create your views here.
+
+@csrf_exempt
+@api_view(["GET", "POST"])
+def diary(request):
+    if(request.method == "POST"):
+        params_json = request.body.decode(encoding = "utf-8")
+        data_json = json.loads(request.body)
+        uid = data_json["uid"]
+        percent = data_json["percent"]
+        text = data_json["text"]
+        emotion = data_json["emotion"]
+        try:
+            user = user_models.User.objects.get(uid=uid)
+            # 감정에 따라서 background 설정한 후 object 저장
+        except:
+            # User does not exists
+            return Response("{Result:Error}")
+
+    elif(request.method == "GET"):
+        print("GET")
