6a1b1d08591a4cac252af9b8f0ff2a72534e7273
younho117 <66814513+younho117@users.noreply.github.com>
2021-01-15 16:58:01 +0900

    logout, UI #4

--- Empathy_Diary/app/src/main/java/com/example/empathy_diary/MainActivity.kt
+++ Empathy_Diary/app/src/main/java/com/example/empathy_diary/MainActivity.kt
@@ -15,10 +15,13 @@ import android.content.Context
 import android.util.Log
 import android.view.Gravity
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.reginald.swiperefresh.CustomSwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,14 +39,53 @@ class MainActivity : AppCompatActivity() {
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
 
-//        val logOut = findViewById<Button>(R.id.logout)
-//        logOut.setOnClickListener(View.OnClickListener {
+        val logOutImg = findViewById<ImageView>(R.id.imageView_logout)
+
+//        logOut_img.setOnClickListener(View.OnClickListener {
 //            Firebase.auth.signOut()
 //            googleSignInClient!!.signOut()
 //            val intent = Intent(this, GoogleLoginActivity::class.java)
 //            startActivity(intent)
 //            finish()
 //        })
+
+        val logOut: View.OnClickListener = View.OnClickListener{
+            Firebase.auth.signOut()
+            googleSignInClient!!.signOut()
+            val intent = Intent(this, GoogleLoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val logOutText = findViewById<TextView>(R.id.textView_logout)
+
+        logOutImg.setOnClickListener(logOut)
+        logOutText.setOnClickListener(logOut)
+
+        val refreshLayout = findViewById<SwipeRefreshLayout>(R.id.swipe_layout)
+
+        refreshLayout.setOnRefreshListener {
+            refreshLayout.isRefreshing = false
+            // 새로운 데이터 받아오기
+        }
+
+
+
+        val similarText = findViewById<TextView>(R.id.textView_Similar)
+        similarText.setOnClickListener{
+            val intent = Intent(this@MainActivity, SimilarDiary::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val differentText = findViewById<TextView>(R.id.textView_Different)
+        differentText.setOnClickListener {
+            val intent = Intent(this@MainActivity, DifferentDiary::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
         findViewById<DrawerLayout>(R.id.drawerLayout).closeDrawer(Gravity.LEFT)
 
         var feedList = arrayListOf<Item_feed>()
@@ -51,7 +93,7 @@ class MainActivity : AppCompatActivity() {
 
         feedList.add(Item_feed("12/21", "날씨가 흐렸다"))
 
-        feedList.add(Item_feed("12/22", "비가\n 왔다"))
+        feedList.add(Item_feed("12/22", "비가\n왔다"))
 
         feedList.add(Item_feed("12/23", "눈\n이\n왔\n다"))
 
@@ -63,13 +105,35 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = linearLayoutManager
         recyclerView.adapter = mAdapter
 
-        findViewById<TextView>(R.id.todayDiary).setOnClickListener(ButtonListener())
+        findViewById<TextView>(R.id.todayDiary).setOnClickListener(ShowCalendar())
+
+        findViewById<TextView>(R.id.textView_myDiary).setOnClickListener(ShowMyDiaries())
+
+//        val refreshLayout = findViewById<CustomSwipeRefreshLayout>(R.id.swipe_layout)
+//
+//        refreshLayout.setOnRefreshListener {
+//            fun onRefresh(){
+//                feedList.add(Item_feed("12/23", "눈\n이\n왔\n다"))
+//            }
+//
+//            // 새로운 데이터 받아오기
+//
+//            refreshLayout.refreshComplete()
+//        }
 
     }
 
-    inner class ButtonListener: View.OnClickListener{
+    inner class ShowCalendar: View.OnClickListener{
         override fun onClick(v: View?) {
             val intent = Intent(this@MainActivity, Calendar::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    inner class ShowMyDiaries: View.OnClickListener{
+        override fun onClick(v: View?){
+            val intent = Intent(this@MainActivity, MyDiaries::class.java)
             startActivity(intent)
             finish()
         }
---
+++ Empathy_Diary/app/src/main/java/com/example/empathy_diary/MyDiaries.kt
@@ -0,0 +1,11 @@
+package com.example.empathy_diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyDiaries : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_diaries)
+    }
+}
