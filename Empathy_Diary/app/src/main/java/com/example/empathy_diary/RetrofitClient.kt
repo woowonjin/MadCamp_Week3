afffdeefd8467c0be25af31b58b2b5650d0a859d
woowonjin <45147364+woowonjin@users.noreply.github.com>
2021-03-07 01:51:24 +0900

    URL 수정

--- Empathy_Diary/app/src/main/java/com/example/empathy_diary/RetrofitClient.kt
+++ Empathy_Diary/app/src/main/java/com/example/empathy_diary/RetrofitClient.kt
@@ -9,7 +9,7 @@ public class RetrofitClient {
     private val gson = GsonBuilder().setLenient().create()
     //서버 주소
 
-    private val BASE_URL = "http://192.249.18.156:8000"
+    private val BASE_URL = "http://ec2-54-180-97-246.ap-northeast-2.compute.amazonaws.com:8000"
 
     val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
